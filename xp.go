93278cf9f459d59a76a44ab93550318b079c0a96
huxiaoping <huxiaoping@xiaomi.com>
2017-04-10 17:09:03 +0800

    添加全局变量

--- xp.go
+++ xp.go
@@ -13,12 +13,12 @@ func(this *Xp) Add(d interface{}) {
 func New() *Xp{
 	x:=new(Xp)
 	x.c = make(map[string]*Controller)
-	x.Config = make(map[string]string)
+	x.config = make(map[string]string)
 	return x
 }
 
 func Run(this *Xp){
-	if addr,ok:=this.Config["addr"];ok{
+	if addr,ok:=this.config["addr"];ok{
 		log.Fatal(http.ListenAndServe(addr,this))
 	}
 	log.Fatal(http.ListenAndServe(addr,this))
@@ -29,4 +29,19 @@ func (this *Xp) Run(addr string){
 
 func (this *Xp) Redirect(url string){
 	http.Redirect(this.Rs,this.Rq,url,301)
+}
+
+func (this *Xp) SetConfig(key,value string)  {
+	this.Lock()
+	defer this.Unlock()
+	this.config[key] = value
+}
+
+func (this *Xp) GetConfig(key string) string {
+	this.RLock()
+	defer this.RUnlock()
+	if v,ok:=this.config[key];ok {
+		return v
+	}
+	return ""
 }
