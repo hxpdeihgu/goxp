34707dca49be4531eb874fb538c16ce212433424
huxiaoping <huxiaoping@xiaomi.com>
2017-04-10 17:51:59 +0800

    添加继承controller

--- controller.go
+++ controller.go
@@ -1,1 +1,35 @@
 package goxp
+
+import "encoding/base64"
+
+type Model struct {
+	
+}
+
+func (this *Model) Md5(s string) string {
+	return _md5(s)
+}
+
+func (this *Model) Sha1(s string) string {
+	return _sha1(s)
+}
+
+func (this *Model) Sha256(s string) string {
+	return _sha256(s)
+}
+
+func (this *Model) Sha512(s string) string {
+	return _sha512(s)
+}
+
+func (this *Model) Base64Decode(s string) (string,error) {
+	v,err:=base64.StdEncoding.DecodeString(s)
+	if err !=nil {
+		return "",err
+	}
+	return string(v),nil
+}
+
+func (this *Model) Base64Encode(s string) string {
+	return base64.StdEncoding.EncodeToString([]byte(s))
+}
